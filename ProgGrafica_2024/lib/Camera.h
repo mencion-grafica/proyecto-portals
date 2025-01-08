4d01d7d05901abbbb588dc2cc2ebd1213028980d
OscarMaestre987 <oscarmaestregonzalez@gmail.com>
2025-01-08 14:35:44 +0100

    aplicar gravedad a los objetos y a la camara

--- ProgGrafica_2024/lib/Camera.h
+++ ProgGrafica_2024/lib/Camera.h
@@ -9,7 +9,9 @@ public:
 	glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f);
 	glm::vec3 front = glm::vec3(0.0f, 0.0f, -1.0f);
 	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::vec3 velocity = glm::vec3(0.0f);
 
+	float gravity = -9.8f;
 	float yaw = -90.0f;
 	float pitch = 0.0f;
 	float lastX = 800.0f / 2.0;
@@ -25,6 +27,10 @@ public:
 	Camera(glm::vec3 position, glm::vec3 front, glm::vec3 up, float fov, float aspectRatio);
 	
 	Camera(glm::vec3 position, glm::vec3 front, glm::vec3 up);
+
+	void jump();
+
+	void portal();
 
 	glm::mat4 computeViewMatrix();
 
